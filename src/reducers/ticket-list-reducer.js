79129c2d4816b2453f32955f991620fc58ebcff7
Neil Bateman <neilbatman@gmail.com>
2019-09-24 20:17:27 -0700

    can push tickets to firebase

---
+++ src/reducers/ticket-list-reducer.js
@@ -0,0 +1,32 @@
+import constants from './../constants';
+const { c } = constants; 
+
+export default (state = {}, action) => {
+  let newState;
+  const { names, location, issue, timeOpen, id, formattedWaitTime } = action;
+
+  switch (action.type) {
+  case c.ADD_TICKET:
+    newState = Object.assign({}, state, {
+      [id]: {
+        names: names,
+        location: location,
+        issue: issue,
+        timeOpen: timeOpen,
+        id: id,
+        formattedWaitTime: formattedWaitTime
+      }
+    });
+    return newState;
+
+  case c.UPDATE_TIME:
+    const newTicket = Object.assign({}, state[id], {formattedWaitTime});
+    newState = Object.assign({}, state, {
+      [id]: newTicket
+    });
+    return newState;
+
+  default:
+    return state;
+  }
+};
